39e780d014ca9ed2d5d1bb1f3209a1a0529a55d2
Gabriel Pupim <gabriel.pda15@gmail.com>
2021-01-17 18:50:02 -0300

    Remoção de dados sensiveis das variaveis

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  loginEnabled: false,
-  emailToken: 'e2a3a65c-dbba-4202-88a3-7c23e78597ff',
-  subscriptionEnabled: true
+  subscriptionEnabled: true,
+  domain: 'randomfeeders.com.br',
+  apiEndpoint: 'http://api.randomfeeders.com.br/'
 };
