ff1ab3fc9eb1d3849305aca407f307a2fb5a55b9
Gabriel Pupim <gabriel.pda15@gmail.com>
2023-07-24 19:00:20 -0300

    Atualização para Angular 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: false, anchorScrolling: 'enabled', relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, { useHash: false, anchorScrolling: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
